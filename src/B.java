4d4becc9e55368e059fcb0d383a45aa3f82ee7a2
HasinduDilesh <128829295+HasinduDilesh@users.noreply.github.com>
2023-05-27 17:44:36 +0530

    Condition Practicals

---
+++ src/B.java
@@ -0,0 +1,15 @@
+public class B {
+    public static void main(String[] args) {
+        String a = "Gawesh";
+
+        if(a == "gawesh") {     // a ==  OR   .eaquals
+            System.out.println("gawesh!");
+        }
+        else if(a == "Gawesh") {
+            System.out.println("Gawesh!");
+        }
+        else {
+            System.out.println("False!");
+        }
+    }
+}
